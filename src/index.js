36eaf6cae3b4d58fc2474137152b5b1544d7dd9e
Arianne <arianne.macena@gmail.com>
2021-07-15 23:09:37 -0300

    adequando enpoints para regras de testes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const port = 3000;
+// const port = 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -16,86 +16,106 @@ function checksExistsUserAccount(request, response, next) {
   const user = users[username];
 
   if(user) {
+    request.user = user;
     return next();
   }
   else {
-    return response.status(400).json({ error: "Usuário inválido (￢_￢)" });
+    return response.status(404).json({ error: "Usuário inválido (￢_￢)" });
   }
 }
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
 
-  const uuid = uuidv4();
-
-  users[username] = { 
-    name, 
-    username, 
-    todos: [], 
-    id: uuidv4() 
-  };
-
-  response.status(201).json({ uuid, username, message: "Usuário criado °˖✧◝(⁰▿⁰)◜✧˖°"});
+  const user = users[username];
+  
+  if(!user) {
+      users[username] = { 
+        name, 
+        username, 
+        todos: [], 
+        id: uuidv4() 
+      };
+    
+      response.status(201).json(users[username]);
+    }
+  else {
+    return response.status(400).json({ error: "Usuário já existe (¯.¯٥)" });
+  }
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  const { username } = request.headers;
+  const { user } = request;
 
-  return response.status(200).json({ todos: users[username].todos });
+  return response.status(200).json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  const { username } = request.headers;
+  const { user } = request;
   const { title, deadline } = request.body;
 
-  const uuid = uuidv4();
-
-  users[username].todos.push({ 
-    id: uuid,
+  const newToDo = { 
+    id: uuidv4(),
     title,
     done: false, 
     deadline: new Date(deadline), 
     created_at: new Date()
-  })
+  };
 
-  response.status(201).json({ uuid, message: "To-Do criado (*￣▽￣)b"});
+  user.todos.push(newToDo);
+
+  response.status(201).json(newToDo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  const { username } = request.headers;
+  const { user } = request;
   const { id } = request.params;
   const { title, deadline } = request.body;
 
-  const todoIndex = users[username].todos.findIndex((todo) => todo.id === id);
+  const todo = user.todos.find((todo) => todo.id === id);
 
-  users[username].todos[todoIndex].deadline = new Date(deadline);
-  users[username].todos[todoIndex].title = title;
-
-  response.sendStatus(204);
+  if(todo) {
+    todo.deadline = new Date(deadline);
+    todo.title = title;
+    response.status(200).json(todo);
+  }
+  else {
+    return response.status(404).json({ error: "To-do não existe (¯.¯٥)" });
+  }
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  const { username } = request.headers;
+  const { user } = request;
   const { id } = request.params;
 
-  const todoIndex = users[username].todos.findIndex((todo) => todo.id === id);
-  users[username].todos[todoIndex].done = true;
-
-  response.sendStatus(204);
+  const todo = user.todos.find((todo) => todo.id === id);
+  
+  if(todo) {
+    todo.done = true;
+    response.status(200).json(todo);
+  }
+  else {
+    return response.status(404).json({ error: "To-do não existe (¯.¯٥)" });
+  }
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  const { username } = request.headers;
+  const { user } = request;
   const { id } = request.params;
 
-  const todoIndex = users[username].todos.findIndex((todo) => todo.id === id);
-  users[username].todos.splice(todoIndex, 1);
+  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
 
-  response.sendStatus(200);
+  if(todoIndex >= 0) {
+    user.todos.splice(todoIndex, 1);
+    response.sendStatus(204);
+  }
+  else {
+    return response.status(404).json({ error: "To-do não existe (¯.¯٥)" });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port} <(￣︶￣)>`)
-})
+// app.listen(port, () => {
+//   console.log(`Servidor rodando na porta ${port} <(￣︶￣)>`)
+// })
 
 module.exports = app;
