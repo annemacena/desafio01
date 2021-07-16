db096f54d496b1b2706779d9bd794dd90b71f4b6
Arianne <arianne.macena@gmail.com>
2021-07-15 21:50:08 -0300

    implementando POST /users

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = {}; // O(1)
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const uuid = uuidv4();
+
+  users[uuid] = { 
+    name, 
+    username, 
+    todos: [], 
+    id: uuidv4() 
+  };
+
+  response.status(201).json({ uuid, message: "Usuário criado °˖✧◝(⁰▿⁰)◜✧˖°"});
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
@@ -37,5 +48,9 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port} <(￣︶￣)>`)
+})
 
 module.exports = app;
